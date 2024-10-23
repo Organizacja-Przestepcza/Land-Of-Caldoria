bd5db56d88b430532658995996ee03d678bb9eea
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-23 21:06:42 +0200

    feat: add exit btn functionality

--- scenes/start.gd
+++ scenes/start.gd
@@ -5,7 +5,10 @@ func _ready() -> void:
 	$MarginContainer/OptionMenu.visible = false
 	$MarginContainer/MainMenu.connect("options_button_pressed",_on_options_button_pressed)
 	$MarginContainer/OptionMenu.connect("backbutton_pressed",_on_options_button_pressed)
+	$MarginContainer/MainMenu.connect("exit_button_pressed", _on_exit_button_pressed)
 	
 func _on_options_button_pressed() -> void:
 	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
 	$MarginContainer/OptionMenu.visible = !$MarginContainer/OptionMenu.visible
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
--- scenes/ui/menu/main_menu.gd
+++ scenes/ui/menu/main_menu.gd
@@ -12,11 +12,17 @@ func _ready() -> void:
 	var OptionsButton = $HBoxContainer/VBoxContainer/OptionButton
 	OptionsButton.set_button_label("Options")
 	
-	
 	var ExitButton = $HBoxContainer/VBoxContainer/ExitButton
 	ExitButton.set_button_label("Exit")
+
 
 func _on_option_button_button_pressed() -> void:
 	emit_signal("options_button_pressed")
 
 signal options_button_pressed
+
+
+func _on_exit_button_button_pressed() -> void:
+	emit_signal("exit_button_pressed")
+
+signal exit_button_pressed
