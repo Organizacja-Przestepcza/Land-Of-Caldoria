88ca90be1c323ad86dba05130c639b8b6a403f85
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-23 09:48:09 +0200

    feat:add options scene

--- scenes/button/custom_button.gd
+++ scenes/button/custom_button.gd
@@ -1,18 +1,11 @@
 extends Control
 
-@export var button_label: String :
-	get:
-		return button_label
-	set(value):
-		button_label = value
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_node("MarginContainer/Label").text = "Custom Button"
+	pass
 
 func set_button_label(new_label: String) -> void:
-	button_label = new_label
-	$MarginContainer/Label.text = button_label
+	$MarginContainer/Label.text = new_label
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -3,18 +3,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var buttonNewGame = $HBoxContainer/VBoxContainer/start_button
-	buttonNewGame.set_button_label("Strat Game")
+	var buttonNewGame = $HBoxContainer/VBoxContainer/StartButton
+	buttonNewGame.set_button_label("Start Game")
 	
-	var buttonLoadGame = $HBoxContainer/VBoxContainer/load_button
+	var buttonLoadGame = $HBoxContainer/VBoxContainer/LoadButton
 	buttonLoadGame.set_button_label("Load Game")
 	
-	var buttonOptions = $HBoxContainer/VBoxContainer/options_button
+	var buttonOptions = $HBoxContainer/VBoxContainer/OptionsButton
 	buttonOptions.set_button_label("Options")
-	
-	var buttonExit = $HBoxContainer/VBoxContainer/exit_button
+	var buttonExit = $HBoxContainer/VBoxContainer/ExitButton
 	buttonExit.set_button_label("Exit")
 	
+func optionMenu () -> void:
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
