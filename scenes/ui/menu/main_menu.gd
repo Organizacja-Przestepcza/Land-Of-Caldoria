c67857f433a074ea76996c629c45d42eae51fac7
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-23 13:21:39 +0200

    chore: backup due to godot crashing

--- scenes/ui/menu/main_menu.gd
+++ scenes/ui/menu/main_menu.gd
@@ -3,17 +3,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var buttonNewGame = $HBoxContainer/VBoxContainer/StartButton
-	buttonNewGame.set_button_label("Start Game")
+	var StartButton = $HBoxContainer/VBoxContainer/StartButton
+	StartButton.set_button_label("Start Game")
 	
-	var buttonLoadGame = $HBoxContainer/VBoxContainer/LoadButton
-	buttonLoadGame.set_button_label("Load Game")
+	var LoadButton = $HBoxContainer/VBoxContainer/LoadButton
+	LoadButton.set_button_label("Load Game")
 	
-	var buttonOptions = $HBoxContainer/VBoxContainer/OptionButton
-	buttonOptions.set_button_label("Options")
+	var OptionButton = $HBoxContainer/VBoxContainer/OptionButton
+	OptionButton.set_button_label("Options")
 	
-	var buttonExit = $HBoxContainer/VBoxContainer/ExitButton
-	buttonExit.set_button_label("Exit")
+	var ExitButton = $HBoxContainer/VBoxContainer/ExitButton
+	ExitButton.set_button_label("Exit")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
