b4a0ef00f079290ef6be29f11415b393ee64900d
ptr47 <zdz437@protonmail.com>
2024-10-23 10:59:40 +0200

    feat: tree with random variants

---
+++ scenes/resource/plant/tree/tree.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var variants = []
+	for tree in $".".get_children():
+		variants.append(tree)
+	var random_variant = variants.pick_random()
+	random_variant.visible = true
+	var collision = random_variant.get_child(1)  # Assuming second child is CollisionShape2D
+	if collision is CollisionPolygon2D:
+		collision.disabled = false  # Enable the collision
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
