61a2fa3895ea9ff36f6207bd90b79b2dc151aa63
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-24 22:13:50 +0200

    feat: add fulscreen functionality

--- scenes/ui/customs/custom_slider.gd
+++ scenes/ui/customs/custom_slider.gd
@@ -1,18 +1,22 @@
 extends Control
-@export var slider_value: int 
-# Funkcja _ready() wykonuje się po załadowaniu sceny
+signal custom_drag_ended
 func _ready():
-	# Inicjalizacja wartości slidera, jeśli potrzebne
 	var slider = $MarginContainer/HSlider
-	slider.value = 100
-	slider_value = slider.value
+	slider.value = 100 
+	
 
 # Getter: Odczytanie wartości suwaka
 func get_slider_value() -> int:
-	var slider = $MarginContainer/Slider
+	var slider = $MarginContainer/HSlider
 	return slider.value
 
 # Setter: Ustawianie wartości suwaka
 func set_slider_value(value: int) -> void:
-	var slider = $MarginContainer/Slider
+	var slider = $MarginContainer/HSlider
 	slider.value = value
+
+	
+
+
+func _on_h_slider_drag_ended(value_changed: bool) -> void:
+	emit_signal("custom_drag_ended")
--- scenes/ui/menu/option_menu.gd
+++ scenes/ui/menu/option_menu.gd
@@ -1,16 +1,18 @@
 extends Control
 
-
+var isFullscreen: bool = false 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	var BackButton = $MarginContainer/VBoxContainer/BackButton
 	BackButton.set_button_label("Go back")
 	
 	var HeaderLabel = $MarginContainer/VBoxContainer/HeaderLabel
-	HeaderLabel.set_button_label("Options")
+	HeaderLabel.get_node("MarginContainer/Label").text = "Options"
 	
 	var MusicSlider = $MarginContainer/VBoxContainer/MusicSlider
-	MusicSlider.get_node("MarginContainer/HSlider").value = 50
+
+	
 	var MusicLabel = $MarginContainer/VBoxContainer/MusicLabel
 	MusicLabel.get_node("MarginContainer/Label").text = ("Music: " + str(MusicSlider.get_node("MarginContainer/HSlider").value) + "%")
 	
@@ -19,9 +21,34 @@ func _ready() -> void:
 	SoundSlider.get_node("MarginContainer/HSlider").value = 75
 	SoundLabel.get_node("MarginContainer/Label").text = ("Sound: " + str(SoundSlider.get_node("MarginContainer/HSlider").value) + "%")
 	
-	 #.text = ("Sound: " + str(SoundSlider.get_node("MarginContainer/HSlider").value) + "%")
+	var ResolutionsOptions = $MarginContainer/VBoxContainer/ResolutionsOptions
+	ResolutionsOptions.get_node("MarginContainer/OptionButton").add_item("1920×1080 (Full HD)", 1)
+	ResolutionsOptions.get_node("MarginContainer/OptionButton").add_item("1366×768",2)
 	
+	var FullscreenButton = $MarginContainer/VBoxContainer/FullscreenButton
+	FullscreenButton.set_button_label("Fullscreen: off")
+	
+	var KeyBindsButton = $MarginContainer/VBoxContainer/KeybindsButton
+	KeyBindsButton.set_button_label("Keybinds")
 
 func _on_back_button_button_pressed() -> void:
 		emit_signal("backbutton_pressed")
 signal backbutton_pressed
+
+
+func _on_fullscreen_button_button_pressed() -> void:
+	isFullscreen = !isFullscreen
+	var FullscreenButton = $MarginContainer/VBoxContainer/FullscreenButton
+	if isFullscreen:
+		FullscreenButton.set_button_label("Fullscreen: on")
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		$MarginContainer/VBoxContainer/ResolutionsOptions.get_node("MarginContainer/OptionButton").disabled = true
+	else:
+		FullscreenButton.set_button_label("Fullscreen: off")
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		$MarginContainer/VBoxContainer/ResolutionsOptions.get_node("MarginContainer/OptionButton").disabled = false
+
+		
+func custom_drag_ended() -> void:
+	var MusicSlider = $MarginContainer/VBoxContainer/MusicSlider
+	$MarginContainer/VBoxContainer/MusicLabel.get_node("MarginContainer/Label").text = ("Music: " + str(MusicSlider.get_node("HSlider").value) + "%")
