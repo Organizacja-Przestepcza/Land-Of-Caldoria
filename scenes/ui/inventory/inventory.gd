3bd35533f07bf6d8639745afd8a281c54a3c509c
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2024-10-27 18:10:47 +0100

    update inventory and add dragging feature

--- scenes/ui/inventory/inventory.gd
+++ scenes/ui/inventory/inventory.gd
@@ -2,22 +2,31 @@ extends CanvasLayer
 
 var invSize: int = 24
 var slotSize: Vector2 = Vector2(64,64)
+var itemsLoad = [
+	"res://items/resource/wood/log.tres"
+]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in invSize:
-		var slot = InventorySlot.new(InventorySlot.Type.MAIN,slotSize)
+		var slot = InventorySlot.new(InventorySlot.Type.MAIN, slotSize)
 		$Main.add_child(slot)
-	var headSlot = InventorySlot.new(InventorySlot.Type.HEAD,slotSize)
-	var torsoSlot = InventorySlot.new(InventorySlot.Type.TORSO,slotSize)
-	var armsSlot = InventorySlot.new(InventorySlot.Type.ARMS,slotSize)
-	var legsSlot = InventorySlot.new(InventorySlot.Type.LEGS,slotSize)
-	var feetSlot = InventorySlot.new(InventorySlot.Type.FEET,slotSize)
+	var headSlot = InventorySlot.new(InventorySlot.Type.HEAD, slotSize)
+	var torsoSlot = InventorySlot.new(InventorySlot.Type.TORSO, slotSize)
+	var armsSlot = InventorySlot.new(InventorySlot.Type.ARMS, slotSize)
+	var legsSlot = InventorySlot.new(InventorySlot.Type.LEGS, slotSize)
+	var feetSlot = InventorySlot.new(InventorySlot.Type.FEET, slotSize)
 	$Armor.add_child(headSlot)
 	$Armor.add_child(torsoSlot)
 	$Armor.add_child(armsSlot)
 	$Armor.add_child(legsSlot)
 	$Armor.add_child(feetSlot)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	for i in itemsLoad.size():
+		var item = InventoryItem.new()
+		item.init(load(itemsLoad[i]))
+		$Main.get_child(i).add_child(item)
+
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		self.visible = !self.visible
--- scenes/ui/inventory/inventory_slot.gd
+++ scenes/ui/inventory/inventory_slot.gd
@@ -2,18 +2,30 @@ class_name InventorySlot
 extends PanelContainer
 
 @export var type: Type
-
-enum Type {MAIN, HEAD, ARMS, TORSO, LEGS, FEET}
+enum Type { MAIN, HEAD, ARMS, TORSO, LEGS, FEET }
 
 func _init(t: Type, cms: Vector2) -> void:
 	type = t
 	custom_minimum_size = cms
+
+
+func _can_drop_data(_at_position: Vector2, data: Variant):
+	if data is InventoryItem:
+		if type == Item.Type.MAIN:
+			if get_child_count() == 0:
+				return true
+			else:
+				if type == data.get_parent().type:
+					return true
+			return get_child(0).data.type == data.data.type
+		else:
+			return data.data.type == type
+	return false
 	
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _drop_data(_at_position: Vector2, data: Variant):
+	if get_child_count() > 0:
+		var item = get_child(0)
+		if item == data:
+			return
+		item.reparent(data.get_parent())
+	data.reparent(self)
