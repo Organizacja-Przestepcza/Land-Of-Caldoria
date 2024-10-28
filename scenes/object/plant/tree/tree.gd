260f95d7d47b4f7c9843ace0dd34813e9a36b3fb
ptr47 <zdz437@protonmail.com>
2024-10-28 12:38:00 +0100

    feat: tree generation

--- scenes/object/plant/tree/tree.gd
+++ scenes/object/plant/tree/tree.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var variants: Array = []
+	
 	for tree in $".".get_children():
 		variants.append(tree)
 	var random_variant = variants.pick_random()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,6 +15,7 @@ func play_animation() -> void:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	# !
 	var animations: Array = ["walk_side", "walk_down", "walk_up", "run_side", "run_down", "run_up"]
 	var i: int = 0
 	if Input.is_action_pressed("sprint"):
@@ -31,3 +32,7 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	play_animation()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		print(self.position)
