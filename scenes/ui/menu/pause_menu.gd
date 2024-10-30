a5e9e40edef316165dbd562b4fc8a0b8d08e8dc8
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-30 19:01:00 +0100

    refactor: buttons are buttoning now

--- scenes/ui/menu/pause_menu.gd
+++ scenes/ui/menu/pause_menu.gd
@@ -4,12 +4,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = false
+	%OptionMenu.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		self.visible = !self.visible
+		$MarginContainer/TextureRect.visible = true 
+		$"%OptionMenu".visible = false
 
 
 func _on_continue_button_pressed() -> void:
@@ -17,4 +20,12 @@ func _on_continue_button_pressed() -> void:
 
 signal quit_pressed
 func _on_quit_button_pressed() -> void:
-	emit_signal("quit_pressed")
+	get_tree().change_scene_to_file("res://scenes/start.tscn")
+
+func _on_options_button_pressed() -> void:
+	$MarginContainer/TextureRect.visible = false
+	%OptionMenu.visible = true
+
+func _on_option_menu_backbutton_pressed() -> void:
+	$MarginContainer/TextureRect.visible = true 
+	$"%OptionMenu".visible = false
