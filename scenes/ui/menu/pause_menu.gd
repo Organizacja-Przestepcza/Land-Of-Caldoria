d42bad4990373e7e6db7d4d5f73c3e6f262d3267
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-10-31 17:20:48 +0100

    refactor: pause menu marginbox del

--- scenes/ui/menu/pause_menu.gd
+++ scenes/ui/menu/pause_menu.gd
@@ -1,6 +1,6 @@
-extends Control
+extends CanvasLayer
 
-
+var paused = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = false
@@ -11,33 +11,38 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
+		pauseGameToggle()
 		self.visible = !self.visible
-		$MarginContainer/TextureRect.visible = true 
+		$TextureRect.visible = true 
 		$"%OptionMenu".visible = false
 		%SaveMenu.visible = false
-
+		
+func pauseGameToggle() -> void:
+	Engine.time_scale = Engine.time_scale * (-1) + 1
+		
 
 func _on_continue_button_pressed() -> void:
 	self.visible = !self.visible
+	pauseGameToggle()
 
 signal quit_pressed
 func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start.tscn")
 
 func _on_options_button_pressed() -> void:
-	$MarginContainer/TextureRect.visible = false
+	$TextureRect.visible = false
 	%OptionMenu.visible = true
 
 func _on_option_menu_backbutton_pressed() -> void:
-	$MarginContainer/TextureRect.visible = true 
+	$TextureRect.visible = true 
 	$"%OptionMenu".visible = false
 
 
 func _on_save_button_pressed() -> void:
-	$MarginContainer/TextureRect.visible = false
+	$TextureRect.visible = false
 	%SaveMenu.visible = true 
 
 
 func _on_save_menu_cancel_pressed() -> void:
-	$MarginContainer/TextureRect.visible = true
+	$TextureRect.visible = true
 	%SaveMenu.visible = false 
