ffab0173ebbc8b8ab10c9ffde568d96fc70c2f3f
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-11-02 22:57:36 +0100

    feat: connect load to main (#32)

--- scenes/start.gd
+++ scenes/start.gd
@@ -4,28 +4,31 @@ extends Control
 func _ready() -> void:
 	$MarginContainer/OptionMenu.visible = false
 	$MarginContainer/NewGameMenu.visible = false
+	$MarginContainer/LoadMenu.visible = false
 	
-	# Connect signals
+
 	$MarginContainer/MainMenu.connect("options_button_pressed", _on_options_button_pressed)
 	$MarginContainer/OptionMenu.connect("backbutton_pressed", _on_options_button_pressed)
 	$MarginContainer/NewGameMenu.connect("backbutton_pressed", _on_start_button_pressed)  
 	$MarginContainer/MainMenu.connect("exit_button_pressed", _on_exit_button_pressed)
 	$MarginContainer/MainMenu.connect("start_button_pressed", _on_start_button_pressed)
+	$MarginContainer/MainMenu.connect("load_button_pressed", _on_load_button_pressed)
+	$MarginContainer/LoadMenu.connect("load_cancel_pressed", _on_load_button_pressed)
 
-# Toggle visibility for Option Menu and Main Menu
 func _on_options_button_pressed() -> void:
 	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
 	$MarginContainer/OptionMenu.visible = !$MarginContainer/OptionMenu.visible
 
-# Quit the game
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
-# Toggle visibility for New Game Menu and Main Menu
+
 func _on_start_button_pressed() -> void:
 	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
 	$MarginContainer/NewGameMenu.visible = !$MarginContainer/NewGameMenu.visible
 
-
-func _on_new_game_menu_backbutton_pressed() -> void:
-	print("back pressed from start")
+func _on_load_button_pressed() -> void:
+	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
+	$MarginContainer/LoadMenu.visible = !$MarginContainer/LoadMenu.visible
+	
--- scenes/ui/menu/load_menu.gd
+++ scenes/ui/menu/load_menu.gd
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_cancel_button_pressed() -> void:
 	emit_signal("load_cancel_pressed")
+
 signal load_cancel_pressed
