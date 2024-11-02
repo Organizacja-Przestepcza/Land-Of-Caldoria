781ab5a402b9928197c35109bbcf9e59e70bbe7e
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-11-02 22:51:45 +0100

    81 enemies (#31)
    * feat: flying blueberries XD
    
    * feat: add area detection and collisions
    
    * fix: collision area and movement
    
    ---------
    
    Co-authored-by: Peter <70442277+ptr47@users.noreply.github.com>

---
+++ scenes/mob/slime.gd
@@ -0,0 +1,19 @@
+extends Enemy
+
+var player
+var chase_player = false
+func _ready() -> void:
+	player = get_parent().get_node("%Player")
+
+func _process(delta: float) -> void:
+	if chase_player:
+		move_towards_player(player, delta)
+	print(chase_player)
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	chase_player = true
+	$AnimatedSprite2D.play("chasing")
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	chase_player = false
+	$AnimatedSprite2D.stop()
