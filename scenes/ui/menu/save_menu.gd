c053141aa9975fd349ad7846514f2156db3c1480
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-11-06 10:31:30 +0100

    save name field works

--- scenes/ui/menu/save_menu.gd
+++ scenes/ui/menu/save_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 var save: SaveGame
 var saves_array = []
+var save_name =""
 func _on_cancel_button_pressed() -> void:
 	emit_signal("save_cancel_pressed")
 signal save_cancel_pressed
@@ -20,7 +21,12 @@ func display_saves() -> void:
 
 func _on_save_button_pressed() -> void:
 	save = SaveGame.new()
-	var filename = save.write_save_game("")
+
+	var filename = save.write_save_game(save_name)
 	if saves_array.find(filename)==-1:
 		saves_array.append(filename)
 		display_saves()
+
+
+func _on_save_name_text_changed(new_text: String) -> void:
+	save_name = new_text
