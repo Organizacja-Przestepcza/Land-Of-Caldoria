392f7bf33667b62c8bc7c3a462db6e7b49d32d8e
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2024-11-16 13:14:44 +0100

    create crab enemy and improve spawning enemies

--- scenes/mob/crab.gd
+++ scenes/mob/crab.gd
@@ -8,15 +8,17 @@ func _ready() -> void:
 	player = get_parent().get_node("%Player")
 
 func _physics_process(delta: float) -> void:
-	print(chase_player)
 	if chase_player:
 		move_towards_player(player, delta)
-		print("Slime: moving")
+		print("Crab: moving")
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	chase_player = true
-	$AnimatedSprite2D.play("chasing")
+	$AnimatedSprite2D.play("moving_top")
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	chase_player = false
 	$AnimatedSprite2D.stop()
+	
+func attack() -> void:
+	$AnimatedSprite2D.play("attack")
