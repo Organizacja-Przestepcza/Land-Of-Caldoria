bb177c9986726b778b3d7283b06b7853e4069855
ptr47 <70442277+ptr47@users.noreply.github.com>
2024-11-18 04:02:40 +0100

    feat: destroying walls

--- scenes/build_manager.gd
+++ scenes/build_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name BuildManager
 
+@onready var building_menu: BuildMenu = $"../Player/Hud/BuildingMenu"
 @onready var build_layer: TileMapLayer = $"../BuildLayer"
 
 var walls: Array = []
@@ -9,9 +10,16 @@ func build():
 	print("Hammer time")
 	var mouse_pos = get_local_mouse_position()
 	var cell_pos = build_layer.local_to_map(mouse_pos)
-	if cell_pos in walls:
-		return
-	print(cell_pos)
-	walls.append(cell_pos)
+	if building_menu.selected_item == 0:
+		if cell_pos in walls:
+			var index = walls.find(cell_pos)
+			print(index)
+			walls.remove_at(index)
+			build_layer.set_cell(cell_pos)
+	elif building_menu.selected_item == 1:
+		if cell_pos in walls:
+			return
+		print(cell_pos)
+		walls.append(cell_pos)
 	build_layer.set_cells_terrain_connect(walls,1,0)
-	#build_layer.set_cell(cell_pos,2,Vector2i(9,14))
+		
--- scenes/ui/hud/building/building.gd
+++ scenes/ui/hud/building/building.gd
@@ -3,13 +3,13 @@ class_name BuildMenu
 
 @onready var hud: Hud = self.get_parent()
 @onready var build_list: ItemList = $MarginContainer/BuildingContainer/BuildList
-var selected_item: int
+var selected_item: int = 0
 var inventory: Dictionary
 
 func open() -> void:
 	self.visible = true
 	inventory = hud.inventory_to_list()
-	update_build_list()
+	#update_build_list()
 
 func close() -> void:
 	self.visible = false
