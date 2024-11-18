9bc975341948d46f96756e34ef6f12ef23ddf463
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2024-11-18 01:33:29 +0100

    add enemy wolf (#60)
    * add enemy wolf
    
    * add mob to game

---
+++ scenes/mob/wolf.gd
@@ -0,0 +1,23 @@
+extends Enemy
+
+var chase_player = false
+func _ready() -> void:
+	health = 20
+	speed = 30
+	strength = 5
+	player = get_parent().get_node("%Player")
+
+func _physics_process(delta: float) -> void:
+	if chase_player:
+		move_towards_player(player, delta)
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	chase_player = true
+	$AnimatedSprite2D.play("walk")
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	chase_player = false
+	$AnimatedSprite2D.stop()
+	
+func attack() -> void:
+	$AnimatedSprite2D.play("attack")
