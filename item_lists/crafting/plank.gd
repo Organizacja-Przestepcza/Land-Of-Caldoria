2a0ac509e73c5cdfe8482eee3fd31d6ba299e282
ptr47 <zdz437@protonmail.com>
2024-11-18 07:41:13 +0100

    fix: plank

--- item_lists/crafting/plank.gd
+++ item_lists/crafting/plank.gd
@@ -2,7 +2,7 @@ extends CraftingRecipe
 
 func _init() -> void:
 	id = "plank"
-	result = ItemLoader.name("stone")
+	result = ItemLoader.name("plank")
 	amount = 2
 	items = {
 	ItemLoader.name("log"): 1
