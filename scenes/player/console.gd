c979c97250a509fbbf28c5e3af8c8376735d054f
ptr47 <70442277+ptr47@users.noreply.github.com>
2024-11-25 04:27:14 +0100

    feat: commands history

--- scenes/player/console.gd
+++ scenes/player/console.gd
@@ -5,6 +5,8 @@ class_name Console
 @onready var player: Player = %Player
 @onready var inventory: Inventory = $"../Inventory"
 var last_state
+var history: PackedStringArray
+var history_index: int = -1
 
 func open():
 	last_state = %Game.state
@@ -21,19 +23,39 @@ func close() -> void:
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	var args = line_edit.text.split(" ",false)
-	args.resize(5)
+	args.resize(3)
 	match args[0]:
-		"teleport": teleport(args.slice(1))
+		"teleport", "tp": teleport(args.slice(1))
 		"give": give(args[1], args[2])
+		"clean": clean()
 		_: print("Unknown command")
+	history.append(line_edit.text)
 	line_edit.clear()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed:
+			match event.keycode:
+				KEY_UP:
+					history_index += 1
+					if history_index < history.size():
+						line_edit.text = history[history_index]
+					else:
+						history_index = -1
+				KEY_DOWN:
+					history_index -= 1
+					if history_index > -1:
+						line_edit.text = history[history_index]
+					else:
+						line_edit.clear()
+
 func teleport(args: PackedStringArray):
-	print(args)
 	if args.size() > 0:
 		if not args[0].is_valid_float():
+			print("\"",args[0], "\" is not a valid number")
 			return
 		if not args[1].is_valid_float():
+			print("\"",args[1], "\" is not a valid number")
 			return
 		get_tree().paused = false
 		player.global_position = Vector2i(args[0].to_float(), args[1].to_float())
@@ -41,11 +63,10 @@ func teleport(args: PackedStringArray):
 		
 func give(item_name: String, amount_str: String):
 	if item_name.is_empty():
-		print("You need to specify an item name")
+		print("Specify an item name")
 		return
 	if not amount_str.is_valid_int():
-		print("You need to specify amount")
-		return
+		amount_str="1"
 	var amount = amount_str.to_int()
 	print(item_name)
 	var item = ItemLoader.name(item_name)
@@ -53,3 +74,6 @@ func give(item_name: String, amount_str: String):
 		inventory.add_item(item,abs(amount))
 	else:
 		print("No such item")
+
+func clean():
+	inventory.clean()
