03d76f5dcc816b00de87da7b870671f338a593b7
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2024-12-09 11:30:03 +0100

    fix multiple quest labels

--- scenes/systems/quests/quest_tab.gd
+++ scenes/systems/quests/quest_tab.gd
@@ -11,9 +11,14 @@ func _process(delta: float) -> void:
 	pass
 	
 func update() -> void:
+	for i in $VBoxContainer.get_child_count():
+		if get_child(i) != null and get_child(i).get_children() != null:
+			var _label = get_child(i).get_child(i)
+			_label.queue_free() #remove VBoxContainer Label
+
 	for quest in Questify.get_quests():
 		var label = Label.new()
-		label.text = "\tQuest: %s \nDescription: %s \nCompleted: %s\n" % [quest.name, quest.description, quest.completed]
+		label.text = "Quest: %s \nDescription: %s \nCompleted: %s\n" % [quest.name, quest.description, quest.completed]
 		$VBoxContainer.add_child(label)
 	
 func open() -> void:
