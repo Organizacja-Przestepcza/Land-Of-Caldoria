4542b67879ed5fefe05a51b72b629ab25575bf6f
ptr47 <70442277+ptr47@users.noreply.github.com>
2024-12-11 00:32:26 +0100

    feat: ranged weapons change

--- items/Ranged.gd
+++ items/Ranged.gd
@@ -1,8 +1,9 @@
 class_name Ranged
 extends Item
 
-@export var damage: int
 @export var durability: int
 @export var knockback: int
 @export var range: float
 @export var cooldown: float
+@export var ammo_list: Array[Item]
+@export var damage_list: Array[int]
