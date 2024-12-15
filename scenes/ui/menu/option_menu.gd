f7f8412fc54bb6b86c96211ddda2ce31ffefef7d
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2024-12-15 23:22:37 +0100

    feat: saving sound and volume value (#102)

--- scenes/ui/menu/option_menu.gd
+++ scenes/ui/menu/option_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
-var is_fullscreen: bool = false 
+var is_fullscreen: bool = false
+var config = ConfigFile.new()
+const SETTINGS_FILE_PATH = "user://settings.cfg"
 
 func initial_controls_setup():
 	
@@ -10,10 +12,29 @@ func initial_controls_setup():
 	%SoundSlider.value = _to_linear(Settings.sound_volume)
 	%SoundLabel.text = ("Sound: " + str(%SoundSlider.value) + "%")
 	
+func settings_controls_setup():
+	if config.has_section_key("MAIN_MENU_SECTION", "MUSIC_SLIDER_VALUE"):
+		%MusicSlider.value = _to_linear(config.get_value("MAIN_MENU_SECTION", "MUSIC_SLIDER_VALUE"))
+		%SoundLabel.text = ("Sound: " + str(%SoundSlider.value) + "%")
+	else:
+		%MusicSlider.value = _to_linear(Settings.music_volume)
+		%MusicLabel .text = ("Music: " + str(%MusicSlider.value) + "%")
+	
+	if config.has_section_key("MAIN_MENU_SECTION", "SOUND_SLIDER_VALUE"):
+		%SoundSlider.value = _to_linear(config.get_value("MAIN_MENU_SECTION", "SOUND_SLIDER_VALUE"))
+		%SoundLabel.text = ("Sound: " + str(%SoundSlider.value) + "%")
+	else:
+		%SoundSlider.value = _to_linear(Settings.sound_volume)
+		%SoundLabel.text = ("Sound: " + str(%SoundSlider.value) + "%")
 	
 
 func _ready() -> void:
-	initial_controls_setup()
+	var err = config.load(SETTINGS_FILE_PATH) # SETTINGS_FILE_PATH = file path string to your settings file
+	if err != OK: 
+		print("opening config file failed" + str(err))
+		initial_controls_setup()
+	else:
+		settings_controls_setup()
 	
 
 func _on_back_button_pressed() -> void:
@@ -35,6 +56,8 @@ func _on_fullscreen_button_pressed() -> void:
 func _on_music_slider_value_changed(value: float) -> void:
 	%MusicLabel.text = "Music: " + str(%MusicSlider.value) + "%"
 	Settings.music_volume = _to_db(value)
+	config.set_value("MAIN_MENU_SECTION", "MUSIC_SLIDER_VALUE", Settings.music_volume)
+	config.save(SETTINGS_FILE_PATH)
 	if get_tree().root.has_node("World"):
 		var world = get_tree().root.get_node("World")
 		world.update_volume()
@@ -42,6 +65,8 @@ func _on_music_slider_value_changed(value: float) -> void:
 func _on_sound_slider_value_changed(value: float) -> void:
 	%SoundLabel.text = "Sound: " + str(%SoundSlider.value) + "%"
 	Settings.sound_volume = _to_db(value)
+	config.set_value("MAIN_MENU_SECTION", "SOUND_SLIDER_VALUE", Settings.sound_volume)
+	config.save(SETTINGS_FILE_PATH)
 
 func _on_keybinds_button_pressed() -> void:
 	pass # Replace with function body.
@@ -50,3 +75,9 @@ func _to_db(value: float) -> float:
 	return lerp(-80, 0, log(1 + value) / log(101))
 func _to_linear(volume_db: float) -> float:
 	return exp((volume_db - (-80)) / (0 - (-80)) * log(101)) - 1
+
+
+func _on_resolution_options_item_selected(index: int) -> void:
+	match(index):
+		_:
+			pass
