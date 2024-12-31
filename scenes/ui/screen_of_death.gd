35ca21d42314bf25eaab9bb03c4e7989e5b2fc81
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-12-31 22:48:38 +0100

    feat: btns fixed

--- scenes/ui/screen_of_death.gd
+++ scenes/ui/screen_of_death.gd
@@ -3,3 +3,8 @@ extends Control
 var start_scene = preload("res://scenes/start.tscn")
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start.tscn")
+
+
+func _on_load_button_pressed() -> void:
+	#SaveManager.load_last_save()
+	pass
