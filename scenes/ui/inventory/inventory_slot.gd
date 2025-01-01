bd66edd716524da1f78ae31ea28611eeca7b8062
ptr47 <70442277+ptr47@users.noreply.github.com>
2025-01-01 04:42:36 +0100

    refactor: storngertyping

--- scenes/ui/inventory/inventory_slot.gd
+++ scenes/ui/inventory/inventory_slot.gd
@@ -11,14 +11,14 @@ func _init(t: Type, cms: Vector2) -> void:
 	custom_minimum_size = cms
 
 
-func _can_drop_data(_at_position: Vector2, item: Variant):
+func _can_drop_data(_at_position: Vector2, item: Variant) -> bool:
 	if item is InventoryItem:
 		if type == Type.MAIN:
 			return true
 		return item.data.slot == type 
 	return false
 	
-func _drop_data(_at_position: Vector2, dropped_item: Variant):
+func _drop_data(_at_position: Vector2, dropped_item: Variant) -> void:
 	if dropped_item is InventoryItem:
 		if get_child_count() > 0:
 			var current_item: InventoryItem = get_child(0)
