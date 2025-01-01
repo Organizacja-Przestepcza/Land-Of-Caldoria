d1ebc214e57fbd3ef408afdfeb65adf847d0d315
ptr47 <70442277+ptr47@users.noreply.github.com>
2025-01-01 03:59:23 +0100

    refactor: saving

--- scenes/save_manager.gd
+++ scenes/save_manager.gd
@@ -1,64 +1,67 @@
-extends Resource
-class_name SaveManager
+extends Node
+#class_name SaveManager
 const SAVE_GAME_DIR = "user://save/"
-static var saves = []
+var _saves: Array[SaveData] = []
 
 func create_save_data() -> SaveData:
-	var world: ProcWorld = WorldData.player.get_parent()
+	var world: ProcWorld = get_tree().root.get_node("World")
 	var s = SaveData.new()
 	s.world_name = WorldData.world_name
 	s.inventory = WorldData.player.inventory.get_data()
 	s.player_global_position = WorldData.player.global_position
 	s.seed = WorldData.seed
 	s.size = WorldData.size
-	s.time = Time.get_datetime_string_from_system()
+	s.time = Time.get_datetime_string_from_system(true,true)
 	world.save_loaded_chunks_objects()
 	s.objects = world.object_tiles.duplicate(true)
 	s.floors = world.floor_tiles.duplicate(true)
 	s.quests = QuestHandler.quest_manager.get_data()
 	return s
 
-func save(name:String) -> String:
-	var save_path
-	if name.is_empty():
-		save_path = str(WorldData.world_name +".tres")
-	else:
-		save_path = str(name + ".tres")
+func save(name:String = "autosave"):
+	if name.is_empty(): name = "autosave"
 	var data = create_save_data()
+	data.save_name = name
+	var save_path: String = "%s_%s_%s.tres"%[data.world_name,data.save_name,data.time]
 	var error = ResourceSaver.save(data, SAVE_GAME_DIR + save_path)
 	if error:
-		print(error)
-	return save_path
+		print_debug(error)
+		return
+	_saves.append(data)
 	
-static func load_game(name:String) -> bool:
-	if not ResourceLoader.exists(SAVE_GAME_DIR + name + ".tres"):
+func load_game(name:String):
+	var name_d: PackedStringArray = name.split(" - ") # 0 worldname, 1 savename, 2 time
+	if not ResourceLoader.exists(SAVE_GAME_DIR + "_".join(name_d) + ".tres"):
 		return false
-	var load_data = ResourceLoader.load(SAVE_GAME_DIR + name + ".tres")
-	if load_data is SaveData:
+	var load_data = _saves.filter(func(element: SaveData): if element.world_name+element.save_name+element.time == "".join(name_d): return element)
+	#var load_data = ResourceLoader.load(SAVE_GAME_DIR + "_".join(name_d) + ".tres") - backup
+	if load_data.front() is SaveData:
+		load_data = load_data.front()
 		WorldData.seed = load_data.seed
 		WorldData.size = load_data.size
 		WorldData.world_name = load_data.world_name
 		WorldData.load = load_data
 		QuestHandler.quest_manager.set_data(load_data.quests)
-		return true
-	printerr("load_data is not SaveData")
-	return false
+		#WorldData.player.get_tree().root.get_node("CaveManager").queue_free()
+		get_tree().change_scene_to_file("res://scenes/world/proc_world.tscn")
 	
-static func remove_save(name: String) -> Error: # returns true if succesful
-	var err = DirAccess.remove_absolute(SAVE_GAME_DIR + name + ".tres")
+func remove_save(name: String) -> Error: # returns true if succesful
+	var name_d: PackedStringArray = name.split(" - ") # 0 worldname, 1 savename, 2 time
+	var err = DirAccess.remove_absolute(SAVE_GAME_DIR + "_".join(name_d) + ".tres")
 	return err
-
-static func load_all() -> void:
-	saves.clear()
+	var load_data = _saves.filter(func(element: SaveData): if element.world_name+element.save_name+element.time == "".join(name_d): return element)
+	_saves.erase(load_data.front())
+	
+func load_all() -> void:
+	_saves.clear()
 	if not DirAccess.dir_exists_absolute(SAVE_GAME_DIR):
 		DirAccess.make_dir_absolute(SAVE_GAME_DIR)
-	var dir = DirAccess.open(SAVE_GAME_DIR)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir():
-				saves.append(file_name)
-			file_name = dir.get_next()
-	else:
-		print("Error occured")
+	for file in DirAccess.get_files_at(SAVE_GAME_DIR):
+		print(file)
+		if not file.get_extension() == "tres":
+			continue
+		var save_data = ResourceLoader.load(SAVE_GAME_DIR+file)
+		_saves.append(save_data)
+
+func get_save_names():
+	return _saves.map(func(element: SaveData): return element.world_name+" - "+element.save_name+" - "+element.time)
--- scenes/ui/menu/save_menu.gd
+++ scenes/ui/menu/save_menu.gd
@@ -2,7 +2,6 @@ extends Control
 
 @onready var load_menu: Control = $"../LoadMenu"
 @onready var saves_list: ItemList = $VBoxContainer/SavesList
-var save_manager: SaveManager
 var save_name:String
 
 func _on_cancel_button_pressed() -> void:
@@ -11,23 +10,19 @@ func _on_cancel_button_pressed() -> void:
 signal save_cancel_pressed
 
 func _ready() -> void:
+	SaveManager.load_all()
 	display_saves()
 
 func display_saves() -> void:
 	saves_list.clear()
-	SaveManager.load_all()
-	for save:String in SaveManager.saves:
-		saves_list.add_item(save.trim_suffix(".tres"))
+	for save:String in SaveManager.get_save_names():
+		saves_list.add_item(save)
 
 
 func _on_save_button_pressed() -> void:
-	WorldData.player = get_parent().get_parent()
-	save_manager = SaveManager.new()
-	
 	if saves_list.is_anything_selected():
 		save_name = saves_list.get_item_text(saves_list.get_selected_items()[0])
-	
-	save_manager.save(save_name)
+	SaveManager.save(save_name)
 	display_saves()
 	load_menu.display_saves()
 
@@ -45,6 +40,7 @@ func _on_delete_button_pressed() -> void:
 		var err = SaveManager.remove_save(selected_item)
 		if err:
 			printerr(error_string(err))
-		display_saves()
-		load_menu.display_saves()
+			return
+		load_menu.load_list.remove_item(saves_list.get_selected_items()[0])
+		saves_list.remove_item(saves_list.get_selected_items()[0])
 	
