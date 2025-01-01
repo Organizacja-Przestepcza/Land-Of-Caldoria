9cd6d5284f76723b50876cfc62657840461099a7
magda_m <79257135+mikemachacky@users.noreply.github.com>
2025-01-01 04:21:11 +0100

    feat: death screen functionality (#105)
    * style: remove unused buttons
    
    * fix: armor bug
    
    * fix: armor bug (for real)
    
    * feat: btns fixed
    
    * refactor: saving
    
    * feat: loading last save
    
    ---------
    
    Co-authored-by: ptr47 <70442277+ptr47@users.noreply.github.com>

--- scenes/ui/menu/load_menu.gd
+++ scenes/ui/menu/load_menu.gd
@@ -1,15 +1,16 @@
 extends Control
+@onready var load_list: ItemList = $VBoxContainer/LoadList
 
 func _ready() -> void:
 	$VBoxContainer/HBoxContainer/LoadButton.disabled = true
+	SaveManager.load_all()
 	display_saves()
 
 	
 func display_saves() -> void:
 	$VBoxContainer/LoadList.clear()
-	SaveManager.load_all()
-	for save: String in SaveManager.saves:
-		$VBoxContainer/LoadList.add_item(save.trim_suffix(".tres"))
+	for save: String in SaveManager.get_save_names():
+		$VBoxContainer/LoadList.add_item(save)
 
 func _on_cancel_button_pressed() -> void:
 	load_cancel_pressed.emit()
@@ -22,10 +23,7 @@ signal load_cancel_pressed
 func _on_load_button_pressed() -> void:
 	var selected_item = $VBoxContainer/LoadList.get_selected_items()[0]
 	var load_name = $VBoxContainer/LoadList.get_item_text(selected_item)
-	if SaveManager.load_game(load_name):
-		get_tree().change_scene_to_packed(load("res://scenes/world/proc_world.tscn"))
-	else:
-		print("Load failed")
+	SaveManager.load_game_by_name(load_name)
 
 
 func _on_load_list_item_selected(_index: int) -> void:
--- scenes/world/world_data.gd
+++ scenes/world/world_data.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var world_name: String
+var world_name: String = "Default World"
 var size: int = 64
 var seed: int = -1
 var player: Player
