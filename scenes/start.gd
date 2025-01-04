55d086070b9762ef0487a95d6e1dab3920d62883
ptr47 <70442277+ptr47@users.noreply.github.com>
2025-01-04 19:10:13 +0100

    refactor: ui edits

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,34 +1,21 @@
 extends Control
+@onready var main_menu: VBoxContainer = $MainMenu
+@onready var new_game_menu: Control = $NewGameMenu
+@onready var load_menu: Control = $LoadMenu
+@onready var option_menu: OptionMenu = $OptionMenu
 
-
-func _ready() -> void:
-	$MarginContainer/OptionMenu.visible = false
-	$MarginContainer/NewGameMenu.visible = false
-	$MarginContainer/LoadMenu.visible = false
+func _on_new_game_button_pressed() -> void:
+	main_menu.visible = not main_menu.visible
+	new_game_menu.visible = not new_game_menu.visible
 	
-
-	$MarginContainer/MainMenu.connect("options_button_pressed", _on_options_button_pressed)
-	$MarginContainer/OptionMenu.connect("backbutton_pressed", _on_options_button_pressed)
-	$MarginContainer/NewGameMenu.connect("backbutton_pressed", _on_start_button_pressed)  
-	$MarginContainer/MainMenu.connect("exit_button_pressed", _on_exit_button_pressed)
-	$MarginContainer/MainMenu.connect("start_button_pressed", _on_start_button_pressed)
-	$MarginContainer/MainMenu.connect("load_button_pressed", _on_load_button_pressed)
-	$MarginContainer/LoadMenu.connect("load_cancel_pressed", _on_load_button_pressed)
-
 func _on_options_button_pressed() -> void:
-	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
-	$MarginContainer/OptionMenu.visible = !$MarginContainer/OptionMenu.visible
-
-
+	main_menu.visible = not main_menu.visible
+	option_menu.visible = not option_menu.visible
+	
+func _on_load_button_pressed() -> void:
+	main_menu.visible = not main_menu.visible
+	load_menu.visible = not load_menu.visible
+	
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_start_button_pressed() -> void:
-	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
-	$MarginContainer/NewGameMenu.visible = !$MarginContainer/NewGameMenu.visible
-
-func _on_load_button_pressed() -> void:
-	$MarginContainer/MainMenu.visible = !$MarginContainer/MainMenu.visible
-	$MarginContainer/LoadMenu.visible = !$MarginContainer/LoadMenu.visible
 	
