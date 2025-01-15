28c2678152056acc015736d59535f3d537dd4d61
ptr47 <70442277+ptr47@users.noreply.github.com>
2025-01-15 13:24:17 +0100

    fix: stacking label not updating

--- scenes/ui/inventory/inventory_slot.gd
+++ scenes/ui/inventory/inventory_slot.gd
@@ -27,7 +27,7 @@ func _drop_data(_at_position: Vector2, dropped_item: Variant) -> void:
 			var current_item: InventoryItem = get_child(0)
 			if current_item == dropped_item:
 				return
-			if current_item.data.name == dropped_item.data.name:
+			if current_item.data == dropped_item.data:
 				var amount_left = current_item.add(dropped_item.count)
 				dropped_item.remove(dropped_item.count - amount_left)
 				return
